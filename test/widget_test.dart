e5d64645d495a35f664379865cbb58c101d47826
Matton giresse <mattongiressem@gmail.com>
2026-03-04 18:03:49 +0100

    Refactor widget test comments to French
    Updated comments to French and clarified test steps.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,19 +11,18 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    //expect(find.text('0'), findsOneWidget);
-    //expect(find.text('1'), findsNothing);
+    // Vérifie que ça commence à 0
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
+    // Simule un tap sur le bouton +
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    //expect(find.text('0'), findsNothing);
-    //expect(find.text('1'), findsOneWidget);
+    // Vérifie que c'est passé à 1
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
   });
 }
